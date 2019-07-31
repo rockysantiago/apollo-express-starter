f199429e1ed4c4887d02360f8e0d4003b67356d4
Rocky Santiago <raphael.santiago@icloud.com>
2019-07-31 13:45:41 +0800

    update tests

--- src/app.spec.js
+++ src/app.spec.js
@@ -3,9 +3,15 @@ import app from './app';
 
 describe('GraphQL /api', () => {
   it('should successfully return "Cardo Dalisay"', async () => {
+    const query = `{
+      me {
+        username
+      }
+    }`;
+
     const response = await request(app)
       .post('/api')
-      .send({ query: '{me {username}}' });
+      .send({ query });
 
     expect(response.status).toBe(200);
 
